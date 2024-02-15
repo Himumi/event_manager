7ee2e8bacb2083d9f65d316d692ef130fdbda8ad
Himumi <erliansyaha@gmail.com>
2024-02-16 03:36:31 +0900

    Build - Get Phone Number

---
+++ lib/event_manager.rb
@@ -0,0 +1,77 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+require 'time'
+
+
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+def phone_numbers(phone_number)
+  number = phone_number.gsub(/\D/, '')
+  if number.length != 10
+    return (number.length == 11) && (number[0] == "1") ? number[1..10] : "bad number"
+  end
+  number
+end
+
+puts 'Event manager Initialized!'
+puts ""
+
+def contents 
+  CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+  )
+end
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+def write_letter
+  contents.each do |row|
+    id = row[0]
+    name = row[:first_name]
+    zipcode = clean_zipcode(row[:zipcode])
+    legislators = legislators_by_zipcode(zipcode)
+    form_letter = erb_template.result(binding)
+    save_thank_you_letter(id,form_letter)
+  end
+end
+
+def get_phone_number
+  contents.each do |row|
+    phone = phone_numbers(row[:homephone])
+    puts phone
+  end
+end
+
+get_phone_number
